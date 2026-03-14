3eed2e921530914f70cb7d7d8088d8bdd73c596b
grGrimales <grojasgrimales@gmail.com>
2026-03-14 07:07:10 -0300

    feat: enhance CORS configuration and implement global validation pipes in main application

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,13 +8,42 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       process.env.FRONTEND_URL,
+      'http://localhost:3000',
       'http://localhost:3001',
-      'http://localhost:3000'
-    ].filter(Boolean),
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
+
+// EXPORT PARA VERCEL
+export default async (req: any, res: any) => {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+  await app.init();
+  const instance = app.getHttpAdapter().getInstance();
+  instance(req, res);
+};
